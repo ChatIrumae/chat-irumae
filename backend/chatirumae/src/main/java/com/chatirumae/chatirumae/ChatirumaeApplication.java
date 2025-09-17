83129db8c41c91d1fa0d01e816136215e9b2e98e
beming-dev <mingfordev@gmail.com>
2025-09-17 16:03:05 +0900

    Add backend project

---
+++ backend/chatirumae/src/main/java/com/chatirumae/chatirumae/ChatirumaeApplication.java
@@ -0,0 +1,13 @@
+package com.chatirumae.chatirumae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatirumaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatirumaeApplication.class, args);
+	}
+
+}
---
+++ backend/chatirumae/src/test/java/com/chatirumae/chatirumae/ChatirumaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatirumae.chatirumae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatirumaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
